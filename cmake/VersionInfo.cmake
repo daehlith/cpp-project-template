6237b497377d7d17bb2eafe12e85f2c56b5d454a
Thomas Dähling <24817+daehlith@users.noreply.github.com>
2021-11-09 21:47:08 +0000

    Add dirty repository information to VersionInfo
    This can be useful to detect if there may be uncommitted changes that are required for a successful build and run.

--- cmake/VersionInfo.cmake
+++ cmake/VersionInfo.cmake
@@ -25,6 +25,20 @@ if(GIT_FOUND)
         WORKING_DIRECTORY ${CMAKE_SOURCE_DIR}
         OUTPUT_VARIABLE GIT_COMMIT_HASH
         OUTPUT_STRIP_TRAILING_WHITESPACE)
+    execute_process(
+        COMMAND ${GIT_EXECUTABLE} diff-index --quiet HEAD --
+        WORKING_DIRECTORY ${CMAKE_SOURCE_DIR}
+        ERROR_QUIET
+        OUTPUT_QUIET
+        RESULT_VARIABLE GIT_UNCOMMITTED_CHANGES
+    )
+    execute_process(
+        COMMAND ${GIT_EXECUTABLE} ls-files --exclude-standard --other --error-unmatch
+        WORKING_DIRECTORY ${CMAKE_SOURCE_DIR}
+        ERROR_QUIET
+        OUTPUT_QUIET
+        RESULT_VARIABLE GIT_UNTRACKED_UNIGNORED_FILES
+    )
 else()
     message(WARNING "Git was not found, version information will not contain git details")
 endif()
@@ -35,8 +49,18 @@ if(NOT GIT_COMMIT_HASH)
     set(GIT_COMMIT_HASH "Unknown commit")
 endif()
 
+set(PROJECT_VERSION_STR "${PROJECT_VERSION} / ${GIT_BRANCH} @ ${GIT_COMMIT_HASH}")
+
+if(GIT_UNTRACKED_UNIGNORED_FILES OR GIT_UNCOMMITTED_CHANGES)
+    set(GIT_DIRTY true)
+    set(PROJECT_VERSION_STR "${PROJECT_VERSION_STR} (dirty)")
+else()
+    set(GIT_DIRTY false)
+endif()
+
 message(STATUS "Git current branch: ${GIT_BRANCH}")
 message(STATUS "Git commit hash: ${GIT_COMMIT_HASH}")
+message(STATUS "Git repository dirty: ${GIT_DIRTY}")
 
 configure_file(
     cmake/version.h.in
