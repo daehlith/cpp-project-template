8211775d5cc279b50986186dda90359fa60bab15
Thomas Dähling <24817+daehlith@users.noreply.github.com>
2021-11-27 17:47:25 +0000

    Provide version information on a per-target basis
    First, this scopes the version information to individual targets, e.g. it uses the provided target's `CMAKE_SOURCE_DIR` and `CMAKE_BINARY_DIR` properties to source git information and decide upon an output directory for the generated header. Secondly, this change removes the need for using `target_link_libraries`. Instead, a simple `#include <version.h>` in a source file of the target is sufficient. Thirdly, this makes version information generation explicit as opposed to have it happen implicitly as side-effect of the `include()` statement.

--- cmake/VersionInfo.cmake
+++ cmake/VersionInfo.cmake
@@ -1,71 +1,78 @@
 #[[
-Generates a `<version.h>` header file with useful build environment and project information, f.e. the version number
-and Git status. See `cmake/version.h.in` for details about available constants.
+Exposes a `target_add_version_info(target)` function that can be used to generate a `<version.h>` header file for the
+given target. The generated `<version.h>` header contains useful build environment and project information, f.e. the
+CMake `project()` details and - if applicable - the Git repository status at the time of building.
 
-Additionally a `VersionInfo` target is provided. Other targets can link against this target to add the relevant include
-directory containing the `<version.h>` header file.
+See `cmake/version.h.in` for details about available constants.
 ]]
 
-if(NOT CMAKE_PROJECT_NAME)
-    message(FATAL_ERROR "Cannot generate version information without a `project()`")
-endif()
+function(target_add_version_info target)
+    message(STATUS "Adding version info to target ${target}")
 
-if(NOT GIT_FOUND)
-    find_package(Git)
-endif()
+    if(NOT PROJECT_NAME)
+        message(FATAL_ERROR "Cannot generate version information without a `project()`")
+    endif()
 
-if(GIT_FOUND)
-    execute_process(
-        COMMAND ${GIT_EXECUTABLE} rev-parse --abbrev-ref HEAD
-        WORKING_DIRECTORY ${CMAKE_SOURCE_DIR}
-        OUTPUT_VARIABLE GIT_BRANCH
-        OUTPUT_STRIP_TRAILING_WHITESPACE)
-    execute_process(
-        COMMAND ${GIT_EXECUTABLE} rev-parse --short HEAD
-        WORKING_DIRECTORY ${CMAKE_SOURCE_DIR}
-        OUTPUT_VARIABLE GIT_COMMIT_HASH
-        OUTPUT_STRIP_TRAILING_WHITESPACE)
-    execute_process(
-        COMMAND ${GIT_EXECUTABLE} diff-index --quiet HEAD --
-        WORKING_DIRECTORY ${CMAKE_SOURCE_DIR}
-        ERROR_QUIET
-        OUTPUT_QUIET
-        RESULT_VARIABLE GIT_UNCOMMITTED_CHANGES
-    )
-    execute_process(
-        COMMAND ${GIT_EXECUTABLE} ls-files --exclude-standard --other --error-unmatch
-        WORKING_DIRECTORY ${CMAKE_SOURCE_DIR}
-        ERROR_QUIET
-        OUTPUT_QUIET
-        RESULT_VARIABLE GIT_UNTRACKED_UNIGNORED_FILES
-    )
-else()
-    message(WARNING "Git was not found, version information will not contain git details")
-endif()
-if(NOT GIT_BRANCH)
-    set(GIT_BRANCH "Unknown branch")
-endif()
-if(NOT GIT_COMMIT_HASH)
-    set(GIT_COMMIT_HASH "Unknown commit")
-endif()
+    if(NOT GIT_FOUND)
+        find_package(Git)
+    endif()
 
-set(PROJECT_VERSION_STR "${PROJECT_VERSION} / ${GIT_BRANCH} @ ${GIT_COMMIT_HASH}")
+    if(GIT_FOUND)
+        get_target_property(TARGET_SOURCE_DIR ${target} SOURCE_DIR)
+        execute_process(
+            COMMAND ${GIT_EXECUTABLE} rev-parse --abbrev-ref HEAD
+            WORKING_DIRECTORY ${TARGET_SOURCE_DIR}
+            OUTPUT_VARIABLE GIT_BRANCH
+            OUTPUT_STRIP_TRAILING_WHITESPACE)
+        execute_process(
+            COMMAND ${GIT_EXECUTABLE} rev-parse --short HEAD
+            WORKING_DIRECTORY ${TARGET_SOURCE_DIR}
+            OUTPUT_VARIABLE GIT_COMMIT_HASH
+            OUTPUT_STRIP_TRAILING_WHITESPACE)
+        execute_process(
+            COMMAND ${GIT_EXECUTABLE} diff-index --quiet HEAD --
+            WORKING_DIRECTORY ${TARGET_SOURCE_DIR}
+            ERROR_QUIET
+            OUTPUT_QUIET
+            RESULT_VARIABLE GIT_UNCOMMITTED_CHANGES
+        )
+        execute_process(
+            COMMAND ${GIT_EXECUTABLE} ls-files --exclude-standard --other --error-unmatch
+            WORKING_DIRECTORY ${TARGET_SOURCE_DIR}
+            ERROR_QUIET
+            OUTPUT_QUIET
+            RESULT_VARIABLE GIT_UNTRACKED_UNIGNORED_FILES
+        )
+    else()
+        message(WARNING "Git was not found, version information will not contain git details")
+    endif()
+    if(NOT GIT_BRANCH)
+        set(GIT_BRANCH "Unknown branch")
+    endif()
+    if(NOT GIT_COMMIT_HASH)
+        set(GIT_COMMIT_HASH "Unknown commit")
+    endif()
 
-if(GIT_UNTRACKED_UNIGNORED_FILES OR GIT_UNCOMMITTED_CHANGES)
-    set(GIT_DIRTY true)
-    set(PROJECT_VERSION_STR "${PROJECT_VERSION_STR} (dirty)")
-else()
-    set(GIT_DIRTY false)
-endif()
+    set(PROJECT_VERSION_STR "${PROJECT_VERSION} / ${GIT_BRANCH} @ ${GIT_COMMIT_HASH}")
 
-message(STATUS "Git current branch: ${GIT_BRANCH}")
-message(STATUS "Git commit hash: ${GIT_COMMIT_HASH}")
-message(STATUS "Git repository dirty: ${GIT_DIRTY}")
+    if(GIT_UNTRACKED_UNIGNORED_FILES OR GIT_UNCOMMITTED_CHANGES)
+        set(GIT_DIRTY true)
+        set(PROJECT_VERSION_STR "${PROJECT_VERSION_STR} (dirty)")
+    else()
+        set(GIT_DIRTY false)
+    endif()
 
-configure_file(
-    cmake/version.h.in
-    .generated/VersionInfo/version.h
-    NEWLINE_STYLE UNIX)
+    message(STATUS "Git current branch: ${GIT_BRANCH}")
+    message(STATUS "Git commit hash: ${GIT_COMMIT_HASH}")
+    message(STATUS "Git repository dirty: ${GIT_DIRTY}")
 
-add_library(VersionInfo INTERFACE)
-target_include_directories(VersionInfo INTERFACE ${CMAKE_BINARY_DIR}/.generated/VersionInfo/)
+    get_target_property(TARGET_BINARY_DIR ${target} BINARY_DIR)
+    set(GENERATED_HEADER_PATH ${TARGET_BINARY_DIR}/.generated/VersionInfo)
+
+    configure_file(
+        cmake/version.h.in
+        ${GENERATED_HEADER_PATH}/version.h
+        NEWLINE_STYLE UNIX)
+
+    target_include_directories(${target} PRIVATE ${GENERATED_HEADER_PATH})
+endfunction()
