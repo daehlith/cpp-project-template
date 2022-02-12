255baed5fca26ca89a4324988dd57eabe940c4c2
Thomas Dähling <24817+daehlith@users.noreply.github.com>
2022-02-12 12:05:09 +0000

    Run doxygen as part of the build
    This enables projects to provide API documentation via source code comments.

--- README.md
+++ README.md
@@ -5,3 +5,14 @@
 - A modern CMake based build-system
 - vcpkg for package management
 - clang-format and editorconfig for source code formatting
+- clang-tidy for linting
+- API documentation via doxygen
+
+## Prerequisites
+
+The template project assumes the following software is available in a build / development environment:
+
+- CMake 3.21 or newer
+- vcpkg
+- A compiler with C++ 17 support
+- doxygen
--- include/bar/bar.h
+++ include/bar/bar.h
@@ -2,4 +2,20 @@
 
 #include <bar/bar_export.h> // Provides symbol visbility helper macros
 
+/*! \file
+ *  \brief This library serves no purposes other than to be an example for structuring a project.
+ *
+ *  OK, that brief description is not entirely correct. It also provides documentation strings to test the
+ *  doxygen integration. Take note that the `\file` directive is necessary to have doxygen include documentation on
+ *  global functions like `printArguments()`.
+ */
+
+/*! \brief A simple utility function to print a program's command-line arguments
+ *
+ * This function's purpose is to simply aid in providing a library example in the template project.
+ * Much like this comment really only provides value in that it demonstrates doxygen integration.
+ *
+ * @param argc Count of command-line arguments passed to the application
+ * @param argv Array with strings representing the command-line arguments passed to the application
+ */
 BAR_EXPORT void printArguments(int argc, char **argv);
